a63869e65b9df9a4f84accfdbab100b68d1a371d
boit <54015841+cheruboit@users.noreply.github.com>
2024-03-21 11:59:20 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+my_list =[10,20,30,40]
+my_list.insert(1,15)
+my_list = [10,15,20,30,40]
+new_items = [50,60,70]
+my_list.extend(new_items)
+my_list.pop()
+my_list.sort()
+index=my_list.index(30)
+print(my_list)
